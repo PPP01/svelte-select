1fe7925f79478bd0c71bc90a4170f1328556c412
Patric Pesch <p.pesch@11com7.de>
2024-11-05 20:00:47 +0100

    Add examples for cssClassesOverwrite prop in Select component
    Created two new example pages demonstrating the use of the cssClassesOverwrite prop in the Select component. One page showcases the prop as a function while the other uses an object for overwriting CSS classes.

---
+++ src/routes/examples/props/cssClassesOverwrite/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Select from '$lib/Select.svelte';
+
+    let items = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' },
+        { value: 'three', label: 'Three' },
+    ];
+
+    const handleCssClassesOverwrite = (cssClasses) => {
+        console.log(cssClasses);
+        cssClasses.base = 'svelte-select myCustomClass';
+        return cssClasses;
+    };
+
+
+</script>
+<h1>Example as function</h1>
+<Select {items} cssClassesOverwrite={handleCssClassesOverwrite} />
+
---
+++ src/routes/examples/props/cssClassesOverwriteObject/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Select from '$lib/Select.svelte';
+
+    let items = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' },
+        { value: 'three', label: 'Three' },
+    ];
+
+    const CssClassesOverwrite = {base: 'svelte-select myCustomClass'}
+</script>
+<h1>Example as Object</h1>
+<Select {items} cssClassesOverwrite={CssClassesOverwrite} />
+
