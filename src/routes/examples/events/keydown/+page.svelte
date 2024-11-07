af30db4ac83f63d8857b230c46b85b00f1d84a6e
Patric Pesch <p.pesch@11com7.de>
2024-11-07 16:12:42 +0100

    Add keydown event handling example in /events/keydown
    Introduce a new example page showcasing how to handle keydown events in Select component. The example demonstrates both default event logging and prevention of default actions.

---
+++ src/routes/examples/events/keydown/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Select from '$lib/Select.svelte';
+
+    let items = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' },
+        { value: 'three', label: 'Three' },
+    ];
+
+    function handle(e) {
+        console.log(e.detail);
+    }
+    function handlePrventDefault(e) {
+        e.preventDefault()
+        console.log(e.detail);
+    }
+</script>
+
+<Select {items} on:keydown={handle} /><br>
+<Select {items} on:keydown={handlePrventDefault} />
+
